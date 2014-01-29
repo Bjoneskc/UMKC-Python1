ba5d1a1a7d7ba941619bb7fd480077d261ce700d
bjoneskc <bjoneskc01@gmail.com>
2014-01-29 17:47:53 -0600

    Uploaded first file

---
+++ test.py
@@ -0,0 +1,36 @@
+__author__ = 'bjones'
+
+# Put complete header at the top of the file
+
+#get input from user
+#Get number of touchdowns
+#Get number of field goals
+#Get number of safeties
+#Calculate total points
+
+#calculate total touch down point, number of touchdown * 7
+#same for the other numbers
+
+#future code - need to check for alpa characters so you don't get an error
+
+touchdowns_str = input("How many touch downs were scored! ")
+fieldgoals_str = input("How man field goals were scored! ")
+safeties_str = input("How many safeties! ")
+
+totalTouchDowns_int = int(touchdowns_str) * 7
+totalFieldGoals_int = int(fieldgoals_str) * 3
+totalSafeties_int = int(safeties_str) * 2
+
+totalScore = totalTouchDowns_int + totalFieldGoals_int + totalSafeties_int
+
+print("Total Score is: ", totalScore)
+
+
+
+
+
+
+
+
+
+
