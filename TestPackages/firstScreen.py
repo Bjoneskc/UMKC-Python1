229f7a4467fbba8b69c90afdcb0ab55db2b98460
bjoneskc <bjoneskc01@gmail.com>
2014-02-14 16:44:07 -0600

    New directory

---
+++ TestPackages/firstScreen.py
@@ -0,0 +1,10 @@
+__author__ = 'bjone'
+
+
+from tkinter import *
+root = Tk()
+
+widget = Label(root)
+widget.config(text="My first GUI!")
+widget.pack(side=TOP, expand=YES, fill=BOTH)
+root.mainloop()
